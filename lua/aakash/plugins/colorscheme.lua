7123d74801ac4aaf8c8e797cfe41bde09449144f
Aakash Kulkarni <aakashkulkarni36@gmail.com>
2024-11-15 00:37:08 -0800

    Added a new color theme and also added a lazygit to the plugins

--- lua/aakash/plugins/colorscheme.lua
+++ lua/aakash/plugins/colorscheme.lua
@@ -1,14 +1,23 @@
+-- -- return {
+-- -- 	{
+-- -- 		--    "rose-pine/neovim", name = "rose-pine",
+-- -- 		--		"everforest",
+-- -- 		"morhetz/gruvbox", -- Add the Gruvbox theme
+-- -- 		priority = 1000, -- make sure to load this before all the other start plugins
+-- -- 		config = function()
+-- -- 			-- load the colorscheme here
+-- -- 			-- vim.cmd("colorscheme rose-pine")
+-- -- 			--			vim.cmd("colorscheme everforest")
+-- -- 			vim.cmd("colorscheme gruvbox")
+-- -- 		end,
+-- -- 	},
+-- }
+
 return {
-	{
-		--    "rose-pine/neovim", name = "rose-pine",
-		--		"everforest",
-		"morhetz/gruvbox", -- Add the Gruvbox theme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			-- vim.cmd("colorscheme rose-pine")
-			--			vim.cmd("colorscheme everforest")
-			vim.cmd("colorscheme gruvbox")
-		end,
-	},
+	"AlexvZyl/nordic.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("nordic").load()
+	end,
 }
